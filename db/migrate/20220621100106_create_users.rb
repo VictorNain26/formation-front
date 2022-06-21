7149d9d5285adb4a096d4bdb8527c4ea80b62fd3
victor lenain <victor.lenain26@gmail.com>
2022-06-21 12:02:37 +0200

    [main/setup-rails] add user model

---
+++ db/migrate/20220621100106_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.date :birthdate
+      t.string :nationality
+      t.text :address
+      t.string :city
+      t.string :zip
+      t.string :phone_number
+      t.string :civility
+
+      t.timestamps
+    end
+  end
+end
