84ecf5cdd1b3bff0b1e696e98a8f6f167f7eaa29
victor lenain <victor.lenain26@gmail.com>
2022-07-19 11:07:47 +0200

    [3/sidebar] some change on JS and add inline svg and starting add I18n

---
+++ app/javascript/inlineSvg.js
@@ -0,0 +1,6 @@
+import inlineSvg from 'https://ga.jspm.io/npm:inline-svg@2.2.3/dist/inlineSVG.min.js';
+
+inlineSvg.init({
+  svgSelector: 'img.svg', // the class attached to all images that should be inlined
+  initClass: 'js-inlinesvg', // class added to <html>
+});
