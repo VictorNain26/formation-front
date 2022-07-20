78e9bd4028cdd74650789d8834c2eb4734d628e7
victor lenain <victor.lenain26@gmail.com>
2022-07-20 17:01:49 +0200

    [4/dashboard] better use of import map

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,4 @@
 # Pin npm packages by running ./bin/importmap
-
-pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
@@ -8,5 +6,4 @@ pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
 pin "inline-svg", to: "https://ga.jspm.io/npm:inline-svg@2.2.3/dist/inlineSVG.min.js", preload: true
 
-pin "inlineSvg"
-pin "inputFile"
+pin_all_from "app/javascript"
