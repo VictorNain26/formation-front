97062559bf7d809cd2834fb836360ea864247af9
victor lenain <victor.lenain26@gmail.com>
2022-07-25 15:46:24 +0200

    [5/profile] add select2 and input integration

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,12 @@
 import "bootstrap";
+import "select2";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const allSelect = document.querySelectorAll(".select");
+
+  allSelect.forEach(((select) => {
+    select.select2({
+      theme: "bootstrap"
+    });
+  }));
+});
